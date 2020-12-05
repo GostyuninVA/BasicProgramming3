8c5825d6201d0edd5998a4c523e7bafa4afca9d0
GostyuninVA <48084264+GostyuninVA@users.noreply.github.com>
2020-12-05 19:48:05 +0300

    Init

---
+++ Assets/Scripts/AssemblyLineMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssemblyLineMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _direction;
+
+    private void Start()
+    {
+        _speed = 1.0f;
+        _direction = Vector3.right;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out CubeMover cubeMover))
+        {
+            cubeMover.SetMoverParamentr(_speed, _direction);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out CubeMover cubeMover))
+        {
+            cubeMover.SetMoverParamentr(0, Vector3.zero);
+        }
+    }
+
+    private void Update()
+    {
+        transform.position += _direction * _speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/CubeMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _direction;
+
+    private void Start()
+    {
+        _speed = 0;
+        _direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.position += _direction * _speed * Time.deltaTime;
+    }
+
+    public void SetMoverParamentr(float speed, Vector3 direction)
+    {
+        _speed = speed;
+        _direction = direction;
+    }
+}
